583c323b425d1e3560a24cfe17df336248933652
Anton Golubeykov <ds6580@yandex.ru>
2022-11-28 23:34:42 +0300

    Added MapVC

---
+++ Moster Busters/UserFlows/MapModule/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  Moster Busters
+//
+//  Created by Антон Голубейков on 28.11.2022.
+//
+
+import UIKit
+import MapKit
+
+final class MapViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet private weak var mapView: MKMapView!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
